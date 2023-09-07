275f36ecf69aff283ffd4a12aad917479c6ea5b2
quangchinh186 <quangchinh1122@gmail.com>
2023-09-07 17:06:48 +0700

    done slide 2 hw

--- app/src/main/java/com/example/homework/MainActivity.java
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.homework;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NumberPicker numberPicker = findViewById(R.id.number);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(1000);
+
+        EditText editText = findViewById(R.id.editTextNumberDecimal);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
